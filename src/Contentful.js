b0e32582a275d2cc9516fa1549f4c5dd6c9279d1
AnnuitasTechDev <calebolojo@annuitas.com>
2019-05-17 20:53:29 -0400

    updated the contentful credentials

--- src/Contentful.js
+++ src/Contentful.js
@@ -3,6 +3,9 @@ import { createClient } from 'contentful'
 
 
 export default createClient({
-    space: process.env.REACT_APP_API_SPACE,
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space: 'm267nzrprnrc',
+    accessToken: 'UhNY7ueYX8L49KUH6vINgsVfgZ555dz-QZyXhQsnQGs'
+
+    // space: process.env.REACT_APP_API_SPACE,
+    // accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
